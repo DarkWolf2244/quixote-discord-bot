abd905180ccfff15fc74c444eb038463a68498f2
DarkWolf <darkwolfx2244@gmail.com>
2022-02-04 18:59:19 +0530

    Added role security

--- commands/setChannel.ts
+++ commands/setChannel.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { writeConfig, readConfig } from "../ConfigManager";
+import { Interaction, MessageInteraction } from "discord.js";
 
 module.exports = {
     name: 'setchannel',
@@ -10,25 +11,29 @@ module.exports = {
         .addStringOption(option => option.setName("channel").setRequired(true).setDescription("The channel to set the current channel to")),
     
     async execute(interaction: any) {
-        console.log(`Now setting ${interaction.options.getString("channel")}`);
-        const channel = interaction.options.getString("channel");
-        const config = readConfig();
-        
-        switch (channel) {
-            case "qotdChannel":
-                config.qotdChannel = interaction.channel.id;
-                break;
-            case "suggestionChannel":
-                config.suggestionChannel = interaction.channel.id;
-                break;
-            case "approvalChannel":
-                config.approvalChannel = interaction.channel.id;
-                break;
-            default:
-                interaction.reply("That doesn't seem to be a valid option. Please use a valid channel type.", { ephemeral: true });
-        }
+        if (interaction.member.roles.cache.has("Quixote Controller")) {
+            console.log(`Now setting ${interaction.options.getString("channel")}`);
+            const channel = interaction.options.getString("channel");
+            const config = readConfig();
+            
+            switch (channel) {
+                case "qotdChannel":
+                    config.qotdChannel = interaction.channel.id;
+                    break;
+                case "suggestionChannel":
+                    config.suggestionChannel = interaction.channel.id;
+                    break;
+                case "approvalChannel":
+                    config.approvalChannel = interaction.channel.id;
+                    break;
+                default:
+                    interaction.reply("That doesn't seem to be a valid option. Please use a valid channel type.", { ephemeral: true });
+            }
 
-        writeConfig(config);
-        interaction.reply(`Successfully set the ${channel} to <#${interaction.channel.id}>!`);
+            writeConfig(config);
+            interaction.reply(`Successfully set the ${channel} to <#${interaction.channel.id}>!`);
+        } else {
+            interaction.reply("You don't have permission to do that!", { ephemeral: true });
+        }
     }
 }
